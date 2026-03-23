9ac9c5daba42b748e9b414edb3d307292e2ad26d
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-03-23 11:48:49 -0300

    Solved checkpoints exercise 13

---
+++ Python/PreCheckpoint01/QuestionThird.py
@@ -0,0 +1,20 @@
+
+import math
+
+valor_a = float(input("Digite o valor A e pressione ENTER: "))
+valor_b = float(input("Digite o valor B e pressione ENTER: "))
+valor_c = float(input("Digite o valor C e pressione ENTER: "))
+delta = (valor_b**2)-(4*(valor_a*valor_c))
+
+if valor_a == 0:
+    print("Sua equação não é do segundo grau por A for igual a zero")
+else:
+
+    if delta < 0:
+        print("Não há raízes reais por delta ser negativo")
+    elif delta == 0:
+       result = -valor_b / 2 * valor_a
+    elif delta > 0:
+        result = -valor_b + math.sqrt(delta) / (2 * valor_a)
+        result2 = -valor_b - math.sqrt(delta) / (2 * valor_a)
+        print(f"Sua raíz 1 deu {result} e {result2}")
