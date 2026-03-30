85dabc5845edd2aa75c6786d66a0ef37f28fe622
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-03-30 09:17:18 -0300

    added checkpoint questions in java

---
+++ Java/ConversoesPyToJ/PyCheckpointOne/CpQuestionOne.java
@@ -0,0 +1,39 @@
+package br.com.fiap.ConversoesPyToJ.PyCheckpointOne;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class CpQuestionOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome e pressione ENTER: ");
+        String nameFound = sc.next();
+
+        System.out.println("Digite o seu peso e pressione ENTER: ");
+        float weightFound = sc.nextFloat();
+
+        System.out.println("Agora digite a sua altura e pressione ENTER: ");
+        float heightFound = sc.nextFloat();
+
+        double imcCalc = weightFound / (Math.pow(heightFound, 2));
+
+        if (imcCalc < 18.5) {
+            String value = "pessoa abaixo do peso";
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado " + value + ".");
+        } else if (imcCalc > 18.5 && imcCalc < 24.9) {
+            String value = "pessoa de peso normal";
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado " + value + ".");
+        } else if (imcCalc > 25.0 && imcCalc < 29.9) {
+            String value = "pessoa sobrepeso";
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado " + value + ".");
+        } else if (imcCalc > 30.0 && imcCalc < 34.9) {
+            String value = "pessoa Obesidade I";
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado " + value + ".");
+        } else if (imcCalc > 35.0 && imcCalc < 39.9) {
+            String value = "pessoa Obesidade II";
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado " + value + ".");
+        } else {
+            System.out.println(nameFound + ", o seu IMC " + imcCalc + " é considerado pessoa Obesidade III (mórbida).");
+        }
+    }
+}
---
+++ Java/ConversoesPyToJ/PyCheckpointOne/CpQuestionTwo.java
@@ -0,0 +1,30 @@
+package br.com.fiap.ConversoesPyToJ.PyCheckpointOne;
+import java.util.Scanner;
+
+public class CpQuestionTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma temperatura e pressione ENTER: ");
+        float tempSelect = sc.nextFloat();
+
+        double tempInFahrenheit = tempSelect * 9/5 + 32;
+        double tempInKelvin = tempSelect + 273.15;
+
+        System.out.println("Digite a opção desejada para conversão\n\n1: Celsius    2: Fahrenheit    3: Kelvin\n\nDigite a opção desejada: ");
+        int optionSelected = sc.nextInt();
+
+        switch (optionSelected) {
+            case 1:
+                System.out.println("Sua temperatura " + tempSelect + " º em Celcius é de " + tempSelect + " ºC");
+                break;
+            case 2:
+                System.out.println("Sua temperatura " + tempSelect + " º em Fahrenheit é de " + tempInFahrenheit + " ºF");
+                break;
+            case 3:
+                System.out.println("Sua temperatura " + tempSelect + " º em Kelvin é de " + tempInKelvin + " ºK");
+                break;
+
+        }
+    }
+}
