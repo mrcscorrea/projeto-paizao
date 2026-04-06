fa91db39c9bd31c564d48a50e6271256b4034b23
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-06 10:08:07 -0300

    Projeto serasa POO

---
+++ Java/br/com/fiap/exercises/poo/Serasa.java
@@ -0,0 +1,48 @@
+package br.com.fiap.exercises.poo;
+
+public class Serasa {
+    private String nome;
+    private int idade;
+    private boolean estaDevendo;
+
+    Serasa(String nome, int idade, boolean estaDevendo) {
+        getNome();
+        getIdade();
+        getDivida();
+    }
+
+    String getNome() {
+        return nome;
+    }
+
+    int getIdade() {
+        return idade;
+    }
+
+    boolean getDivida() {
+        return estaDevendo;
+    }
+
+    void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    void setDivida(boolean estaDevendo) {
+        this.estaDevendo = estaDevendo;
+    }
+
+    void consultarNome() {
+        System.out.println("=== SERASA CONSULTAS ===");
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        if (!estaDevendo) {
+            System.out.println("Seu cadastro se encontra limpo e em estado regular.");
+        } else {
+            System.out.println("Seu cadastro se enconta irregular e com dívidas presentes.");
+        }
+    }
+}
