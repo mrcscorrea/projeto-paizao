09de47317d21b8449137d67ca8d9d781b8cc811f
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-09 11:57:40 -0300

    Create ProdutoCollectionRepository.java

---
+++ Java/br/com/fiap/exercises/poo/project/ProdutoCollectionRepository.java
@@ -0,0 +1,57 @@
+package br.com.fiap.project;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.List;
+
+public class ProdutoCollectionRepository {
+    private static List<Produto> produtos;
+
+    static {
+        produtos = new Vector<>();
+        Produto kindle = new Produto();
+        kindle.setCategoria(CategoriaCollectionRepository.findById(1l))
+                .setNome("Kindle")
+                .setDescricao("e-reader da Amazon")
+                .setDataCadastro(LocalDateTime.now())
+                .setPreco(BigDecimal.valueOf(899.99));
+
+        Produto iphone = new Produto();
+        iphone.setCategoria(CategoriaCollectionRepository.findById(2l))
+                .setNome("Iphone 14 Pro Max")
+                .setDescricao("Aparelho celular de última geração da Apple")
+                .setDataCadastro(LocalDateTime.now())
+                .setPreco(BigDecimal.valueOf(12999.99));
+
+        Arrays.asList(kindle, iphone).forEach(ProdutoCollectionRepository::save);
+    }
+
+    public static Produto save(Produto produto) {
+        if (!produtos.contains(produto)) {
+            produto.setId((long) produtos.size() + 1);
+            produtos.add(produto);
+            return produto;
+        } else {
+            return null;
+        }
+    }
+
+    public static Produto findById(Long id) {
+        return produtos.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static List<Produto> findByCategoria(Categoria categoria) {
+        return produtos.stream()
+                .filter(p -> p.setCategoria().equals(categoria))
+                .toList();
+    }
+
+    public static List<Produto> findAll() {
+        return produtos;
+    }
+}
