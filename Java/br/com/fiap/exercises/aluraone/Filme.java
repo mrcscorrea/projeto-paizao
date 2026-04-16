c86be43c50169d446e5e006703e25120c7d934ee
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-16 11:50:35 -0300

    adicionado exercícios poo da alura

---
+++ Java/br/com/fiap/exercises/aluraone/Filme.java
@@ -0,0 +1,8 @@
+package exercises.aluraone;
+
+public class Filme extends Titulo {
+    private String diretor;
+
+    public String getDiretor() { return this.diretor; }
+    public void setDiretor(String d) { this.diretor = d; }
+}
