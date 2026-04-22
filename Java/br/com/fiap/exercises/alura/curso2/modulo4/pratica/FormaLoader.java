d8723b3c4d8e78836ba021a27d28a039bfcb2102
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-22 13:00:10 -0300

    Update FormaLoader.java

--- Java/br/com/fiap/exercises/alura/curso2/modulo4/pratica/FormaLoader.java
+++ Java/br/com/fiap/exercises/alura/curso2/modulo4/pratica/FormaLoader.java
@@ -4,9 +4,9 @@ public class FormaLoader {
     public static void main(String[] args) {
 
         Quadrado q1 = new Quadrado(2);
-        q1.calcularArea();
+        System.out.println("Área do Quadrado: " + q1.calcularArea());
 
         Circulo c1 = new Circulo(4);
-        c1.calcularArea();
+        System.out.println("Área do Círculo: "+ c1.calcularArea());
     }
 }
