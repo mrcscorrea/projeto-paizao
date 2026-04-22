c7696c46fa9bbf51535acd81abdf434676b7c274
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-22 12:57:07 -0300

    Create FormaLoader.java

---
+++ Java/br/com/fiap/exercises/alura/curso2/modulo4/pratica/FormaLoader.java
@@ -0,0 +1,12 @@
+package br.com.fiap.exercises.alura.curso2.modulo4.pratica;
+
+public class FormaLoader {
+    public static void main(String[] args) {
+
+        Quadrado q1 = new Quadrado(2);
+        q1.calcularArea();
+
+        Circulo c1 = new Circulo(4);
+        c1.calcularArea();
+    }
+}
