6f5a007ec4ca09dab684f8ddbdb20d748d5353e2
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-27 11:32:30 -0300

    Create exercicio_3.py

---
+++ Python/FunctionQuestions/exercicio_3.py
@@ -0,0 +1,9 @@
+numero = int(input("Digite um número inteiro e pressione ENTER: "))
+
+def dobrar(n):
+    n = numero
+
+    numero_2 = n**2
+    return numero_2
+
+print(f" seu número {numero} dorbado é de {dobrar(numero)}")
