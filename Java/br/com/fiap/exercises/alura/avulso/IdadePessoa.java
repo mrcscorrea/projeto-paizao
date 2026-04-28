8422139d93f3a62e0ded617faf7df26a4dfeeabe
mrcscorrea <112769781+mrcscorrea@users.noreply.github.com>
2026-04-28 17:30:59 -0300

    Create IdadePessoa.java

---
+++ Java/br/com/fiap/exercises/alura/avulso/IdadePessoa.java
@@ -0,0 +1,41 @@
+package br.com.fiap.exercises.alura.avulso;
+
+public class IdadePessoa {
+    private String nome;
+    private int idade;
+
+    public IdadePessoa() {}
+
+    public IdadePessoa(String n, int i) {
+        setNome(n);
+        setIdade(i);
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    void setNome(String n) {
+        this.nome = n;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    void setIdade(int i) {
+        this.idade = i;
+    }
+
+
+    public void verificarIdade() {
+        if (getIdade() < 0) {
+            System.out.println("Idade inválida");
+        }
+        if (getIdade() < 18) {
+            System.out.println(getNome()+" é menor de idade.");
+        } else {
+            System.out.println(getNome()+" é maior de idade.");
+        }
+    }
+}
